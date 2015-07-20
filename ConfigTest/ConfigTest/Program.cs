9da7233d58520c63ec1a3ca8e2e5e04f4bcac285
sharkbound <laptopblaster@gmail.com>
2015-07-20 09:14:43 -0500

    refactored code to be better and cleaner

--- ConfigTest/ConfigTest/Program.cs
+++ ConfigTest/ConfigTest/Program.cs
@@ -14,68 +14,40 @@ namespace ConfigTest
             list();
             string username = "";
             string password = "";
+            var loginmenu = new Action(LoggedIn);
             ConsoleKeyInfo key;
             begin:
-            Console.WriteLine("do you want to register or login?\n\n1:\t-\tRegister\n2:\t-\tLogin using existing account");
-            key = Console.ReadKey();
+            key = RegisterOrLogin();
             switch (key.Key)
             {
                 case ConsoleKey.D1:
                     #region register
-                    clear();
-                    Console.WriteLine("enter the username for the new user...");
-                    Settings.Default.username2 = Console.ReadLine();
-                    clear();
-                    Console.WriteLine("enter the password for the new user...");
-                    Settings.Default.password2 = Console.ReadLine();
-                    clear();
-                    Console.WriteLine("enter the email for the new user...");
-                    Settings.Default.email2 = Console.ReadLine();
-                    clear();
-                    Settings.Default.Save();
+                    Register();
                     goto begin;
                     #endregion
                 case ConsoleKey.D2:
                     #region login
-                    Console.WriteLine("login user first account or second account?");
-                    Console.WriteLine("\n\n1:\t-   login using first account\n2:\t-   login user registered account (second account)");
-                    key = Console.ReadKey();
+                    key = FirstLogin();
                     if (key.Key == ConsoleKey.D1)
                     {
-                        clear();
-                        Console.WriteLine("enter your username then press enter...");
-                        username = Console.ReadLine();
-                        clear();
-                        Console.WriteLine("enter your password then press enter...");
-                        password = Console.ReadLine();
+                        Login(ref username, ref password);
                         if (password == Settings.Default.password && username == Settings.Default.username)
                         {
                             clear();
                         loggedin:
-                            Console.ForegroundColor = ConsoleColor.Cyan;
-                            Console.WriteLine("you are logged in as:  {0}", Settings.Default.username);
-                            Console.ForegroundColor = ConsoleColor.White;
+                            LoggedIn();
                             if (Settings.Default.email == "d")
                             {
-                                Console.WriteLine("enter your email then press enter");
-                                string temp = Console.ReadLine();
-                                Settings.Default.email = temp;
-                                Console.WriteLine("email for {0} was set to {1}", Settings.Default.username, Settings.Default.email);
-                                Settings.Default.Save();
+                                ChangeEmail();
                             }
-                            Console.WriteLine("\n\nwhat do you want to do?");
-                            Console.WriteLine("1:\tchange password\n2:\tchange username");
-                            key = Console.ReadKey();
+                            key = LoggedInMenu(key);
                             if (key.Key == ConsoleKey.D1)
                             {
-                                Console.WriteLine("enter your email to confirm you are you");
-                                string email2 = Console.ReadLine();
+                                string email2 = EmailVerify();
                                 if (email2 == Settings.Default.email)
                                 {
-                                    Console.WriteLine("enter the new password");
-                                    string newpass = Console.ReadLine();
-                                    Settings.Default.password = newpass;
-                                    Settings.Default.Save();
+                                    ChangeUserOnePassword();
+                                    //loginmenu();
                                     goto loggedin;
                                 }
                                 else
@@ -87,14 +59,10 @@ namespace ConfigTest
                             }
                             else if (key.Key == ConsoleKey.D2)
                             {
-                                Console.WriteLine("enter your email to confirm you are you");
-                                string email3 = Console.ReadLine();
+                                string email3 = UserOneEmailConfirm();
                                 if (email3 == Settings.Default.email)
                                 {
-                                    Console.WriteLine("enter the new username");
-                                    string newusername = Console.ReadLine();
-                                    Settings.Default.username = newusername;
-                                    Settings.Default.Save();
+                                    ChangeUsername();
                                     goto loggedin;
                                 }
                                 else
@@ -112,10 +80,7 @@ namespace ConfigTest
                         }
                         else
                         {
-                            Console.ForegroundColor = ConsoleColor.Red;
-                            clear();
-                            Console.WriteLine("username \"{0}\" and password \"{1}\" do not match any accounts", username, password);
-                            Console.ForegroundColor = ConsoleColor.White;
+                            BadLogin(username, password);
                             goto begin;
                         }
                     }
@@ -124,39 +89,23 @@ namespace ConfigTest
                     else // second users login
                     {
                         clear();
-                        Console.WriteLine("enter your username then press enter...");
-                        username = Console.ReadLine();
-                        clear();
-                        Console.WriteLine("enter your password then press enter...");
-                        password = Console.ReadLine();
+                        UserTwoLogin(ref username, ref password);
                         if (password == Settings.Default.password2 && username == Settings.Default.username2)
                         {
                             clear();
                         loggedin:
-                            Console.ForegroundColor = ConsoleColor.Cyan;
-                            Console.WriteLine("you are logged in as:  {0}", Settings.Default.username2);
-                            Console.ForegroundColor = ConsoleColor.White;
+                            UserTwoLoginDialog();
                             if (Settings.Default.email2 == "d")
                             {
-                                Console.WriteLine("enter your email then press enter");
-                                string temp = Console.ReadLine();
-                                Settings.Default.email2 = temp;
-                                Console.WriteLine("email for {0} was set to {1}", Settings.Default.username2, Settings.Default.email2);
-                                Settings.Default.Save();
+                                ChangeUserTwoDefaultEmail();
                             }
-                            Console.WriteLine("\n\nwhat do you want to do?");
-                            Console.WriteLine("1:\tchange password\n2:\tchange username");
-                            key = Console.ReadKey();
+                            key = UserTwoLoginMenu();
                             if (key.Key == ConsoleKey.D1)
                             {
-                                Console.WriteLine("enter your email to confirm you are you");
-                                string email2 = Console.ReadLine();
+                                string email2 = UserTwoEmailConfirm();
                                 if (email2 == Settings.Default.email2)
                                 {
-                                    Console.WriteLine("enter the new password");
-                                    string newpass = Console.ReadLine();
-                                    Settings.Default.password2 = newpass;
-                                    Settings.Default.Save();
+                                    ChangeUserTwoPassword();
                                     goto loggedin;
                                 }
                                 else
@@ -168,14 +117,12 @@ namespace ConfigTest
                             }
                             else if (key.Key == ConsoleKey.D2)
                             {
-                                Console.WriteLine("enter your email to confirm you are you");
-                                string email3 = Console.ReadLine();
+                                //Console.WriteLine("enter your email to confirm you are you");
+                                //string email3 = Console.ReadLine();
+                                string email3 = UserTwoEmailConfirm();
                                 if (email3 == Settings.Default.email2)
                                 {
-                                    Console.WriteLine("enter the new username");
-                                    string newusername = Console.ReadLine();
-                                    Settings.Default.username2 = newusername;
-                                    Settings.Default.Save();
+                                    UserTwoChangeUsername();
                                     goto loggedin;
                                 }
                                 else
@@ -193,25 +140,196 @@ namespace ConfigTest
                         }
                         else
                         {
-                            Console.ForegroundColor = ConsoleColor.Red;
-                            clear();
-                            Console.WriteLine("username \"{0}\" and password \"{1}\" do not match any accounts", username, password);
-                            Console.ForegroundColor = ConsoleColor.White;
+                            UserTwoBadLogin(username, password);
                             goto begin;
                         }
                     }
                     #endregion
                 default:
                     #region invalid selection
-                    Console.ForegroundColor = ConsoleColor.Red;
-                    clear();
-                    Console.WriteLine("{0} is not a valid option!", key.Key);
-                    Console.ForegroundColor = ConsoleColor.White;
+                    key = InvalidFirstMenuSelection(key);
                     goto begin;
                     #endregion
             }
         }
-        #region methods
+
+        private static void UserTwoBadLogin(string username, string password)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            clear();
+            Console.WriteLine("username \"{0}\" and password \"{1}\" do not match any accounts", username, password);
+            Console.ForegroundColor = ConsoleColor.White;
+        }
+
+        private static void UserTwoChangeUsername()
+        {
+            Console.WriteLine("enter the new username");
+            string newusername = Console.ReadLine();
+            Settings.Default.username2 = newusername;
+            Settings.Default.Save();
+        }
+
+        private static void ChangeUserTwoPassword()
+        {
+            Console.WriteLine("enter the new password");
+            string newpass = Console.ReadLine();
+            Settings.Default.password2 = newpass;
+            Settings.Default.Save();
+        }
+
+        private static string UserTwoEmailConfirm()
+        {
+            Console.WriteLine("enter your email to confirm you are you");
+            string email2 = Console.ReadLine();
+            return email2;
+        }
+
+        private static ConsoleKeyInfo UserTwoLoginMenu()
+        {
+            ConsoleKeyInfo key;
+            Console.WriteLine("\n\nwhat do you want to do?");
+            Console.WriteLine("1:\tchange password\n2:\tchange username");
+            key = Console.ReadKey();
+            return key;
+        }
+
+        private static void ChangeUserTwoDefaultEmail()
+        {
+            Console.WriteLine("enter your email then press enter");
+            string temp = Console.ReadLine();
+            Settings.Default.email2 = temp;
+            Console.WriteLine("email for {0} was set to {1}", Settings.Default.username2, Settings.Default.email2);
+            Settings.Default.Save();
+        }
+
+        private static void UserTwoLoginDialog()
+        {
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.WriteLine("you are logged in as:  {0}", Settings.Default.username2);
+            Console.ForegroundColor = ConsoleColor.White;
+        }
+
+        private static void UserTwoLogin(ref string username, ref string password)
+        {
+            Console.WriteLine("enter your username then press enter...");
+            username = Console.ReadLine();
+            clear();
+            Console.WriteLine("enter your password then press enter...");
+            password = Console.ReadLine();
+        }
+
+        private static ConsoleKeyInfo FirstLogin()
+        {
+            ConsoleKeyInfo key;
+            Console.WriteLine("login user first account or second account?");
+            Console.WriteLine("\n\n1:\t-   login using first account\n2:\t-   login user registered account (second account)");
+            key = Console.ReadKey();
+            return key;
+        }
+
+        private static ConsoleKeyInfo InvalidFirstMenuSelection(ConsoleKeyInfo key)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            clear();
+            Console.WriteLine("{0} is not a valid option!", key.Key);
+            Console.ForegroundColor = ConsoleColor.White;
+            return key;
+        }
+
+        private static void ChangeUserOnePassword()
+        {
+            Console.WriteLine("enter the new password");
+            string newpass = Console.ReadLine();
+            Settings.Default.password = newpass;
+            Settings.Default.Save();
+        }
+
+        private static string UserOneEmailConfirm()
+        {
+            Console.WriteLine("enter your email to confirm you are you");
+            string email3 = Console.ReadLine();
+            return email3;
+        }
+
+        private static void ChangeUsername()
+        {
+            Console.WriteLine("enter the new username");
+            string newusername = Console.ReadLine();
+            Settings.Default.username = newusername;
+            Settings.Default.Save();
+        }
+
+        private static void BadLogin(string username, string password)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            clear();
+            Console.WriteLine("username \"{0}\" and password \"{1}\" do not match any accounts", username, password);
+            Console.ForegroundColor = ConsoleColor.White;
+        }
+
+        private static void Login(ref string username, ref string password)
+        {
+            clear();
+            Console.WriteLine("enter your username then press enter...");
+            username = Console.ReadLine();
+            clear();
+            Console.WriteLine("enter your password then press enter...");
+            password = Console.ReadLine();
+        }
+
+        private static string EmailVerify()
+        {
+            Console.WriteLine("enter your email to confirm you are you");
+            string email2 = Console.ReadLine();
+            return email2;
+        }
+
+        private static ConsoleKeyInfo LoggedInMenu(ConsoleKeyInfo key)
+        {
+            Console.WriteLine("\n\nwhat do you want to do?");
+            Console.WriteLine("1:\tchange password\n2:\tchange username");
+            key = Console.ReadKey();
+            return key;
+        }
+
+        private static void ChangeEmail()
+        {
+            Console.WriteLine("enter your email then press enter");
+            string temp = Console.ReadLine();
+            Settings.Default.email = temp;
+            Console.WriteLine("email for {0} was set to {1}", Settings.Default.username, Settings.Default.email);
+            Settings.Default.Save();
+        }
+
+        private static void LoggedIn()
+        {
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.WriteLine("you are logged in as:  {0}", Settings.Default.username);
+            Console.ForegroundColor = ConsoleColor.White;
+        }
+
+        private static void Register()
+        {
+            clear();
+            Console.WriteLine("enter the username for the new user...");
+            Settings.Default.username2 = Console.ReadLine();
+            clear();
+            Console.WriteLine("enter the password for the new user...");
+            Settings.Default.password2 = Console.ReadLine();
+            clear();
+            Console.WriteLine("enter the email for the new user...");
+            Settings.Default.email2 = Console.ReadLine();
+            clear();
+            Settings.Default.Save();
+        }
+
+        private static ConsoleKeyInfo RegisterOrLogin()
+        {
+            ConsoleKeyInfo key;
+            Console.WriteLine("do you want to register or login?\n\n1:\t-\tRegister\n2:\t-\tLogin using existing account");
+            key = Console.ReadKey();
+            return key;
+        }
         static void clear()
         {
             Console.Clear();
@@ -230,6 +348,5 @@ namespace ConfigTest
             Console.WriteLine("******************************************************");
             Console.ForegroundColor = ConsoleColor.White;
         }
-        #endregion
     }
 }
