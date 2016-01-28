9c9da849ffbd331313265037d0743a2f6dcd49fd
sharkbound <laptopblaster@gmail.com>
2016-01-27 20:45:44 -0800

    added /duty

---
+++ godmodeAnnouncer/godmodeAnnouncer/CommandDuty.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Rocket.API;
+using Rocket.Core.Logging;
+using Rocket.Unturned.Player;
+using Rocket.Unturned.Chat;
+
+namespace godmodeAnnouncer
+{
+    class CommandDuty : IRocketCommand
+    {
+        public List<string> Aliases
+        {
+            get { return new List<string>() { "duty" }; }
+        }
+
+        public AllowedCaller AllowedCaller
+        {
+            get { return Rocket.API.AllowedCaller.Player; }
+        }
+
+        public void Execute(IRocketPlayer caller, string[] command)
+        {
+            UnturnedPlayer unturnedCaller = (UnturnedPlayer)caller;
+
+            if (caller.IsAdmin)
+            {
+                unturnedCaller.Admin(false);
+            }
+            else
+            {
+                unturnedCaller.Admin(true);
+            }
+        }
+
+        public string Help
+        {
+            get { return "puts a admin on or off duty"; }
+        }
+
+        public string Name
+        {
+            get { return "duty"; }
+        }
+
+        public List<string> Permissions
+        {
+            get { return new List<string>() {"duty"}; }
+        }
+
+        public string Syntax
+        {
+            get { return ""; }
+        }
+    }
+}
