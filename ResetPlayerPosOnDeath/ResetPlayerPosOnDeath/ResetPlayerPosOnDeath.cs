55db9c42b2f23cf5f057be44bcfd6236b06e4491
sharkbound <laptopblaster@gmail.com>
2016-02-27 13:41:27 -0800

    added a plugin to auto-delete bugged players player.dat file to fix them

---
+++ ResetPlayerPosOnDeath/ResetPlayerPosOnDeath/ResetPlayerPosOnDeath.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Rocket.API;
+using Rocket.Core.Plugins;
+using Rocket.Core.Logging;
+using Rocket.Unturned;
+using Rocket.Unturned.Player;
+using SDG.Unturned;
+using System.IO;
+
+namespace ResetPlayerPosOnDeath
+{
+    class ResetPlayerPosOnDeath : RocketPlugin<ResetPlayerPosOnDeathConfig>
+    {
+        public static ResetPlayerPosOnDeath Instance;
+
+        public string playersFolderPath;
+
+        protected override void Load()
+        {
+            Instance = this;
+
+            U.Events.OnPlayerDisconnected += Events_OnPlayerDisconnected;
+
+            playersFolderPath = ResetPlayerPosOnDeath.Instance.Configuration.Instance.ServerPath + @"\Players\";
+
+            Logger.Log("ResetPlayerPosOnDeath has loaded!");
+            Logger.Log("ServerPath: " + ResetPlayerPosOnDeath.Instance.Configuration.Instance.ServerPath);
+            Logger.Log("players Folder Path: " + playersFolderPath);
+        }
+
+        protected override void Unload()
+        {
+            Logger.Log("ResetPlayerPosOnDeath has Unloaded!");
+        }
+
+        public void Events_OnPlayerDisconnected(UnturnedPlayer player)
+        {
+            string[] directories =  System.IO.Directory.GetDirectories(playersFolderPath);
+
+            bool playerIdIsFoundInList = false;
+
+            foreach (string id in Instance.Configuration.Instance.SteamIds)
+            {
+                if (player.Id == id)
+                {
+                    foreach (string s in directories)
+                    {
+                        if (s.Contains(player.Id))
+                        {
+                            //Logger.Log("found player directory for " + player.Id + " at:");
+                            //Logger.Log(s);
+
+                            string deleteDir = s + @"\" + Provider.map + @"\Player\" + "Player.dat";
+                            //Logger.LogError(deleteDir);
+
+                            if (File.Exists(deleteDir))
+                            {
+                                Logger.Log("deleting player.dat file for player:" + "SteamId- " + player.Id + " / name- " + player.DisplayName);
+                                File.Delete(s + @"\" + Provider.map + @"\Player\" + "Player.dat"); 
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ ResetPlayerPosOnDeath/ResetPlayerPosOnDeath/ResetPlayerPosOnDeathConfig.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Rocket.API;
+using System.Xml.Serialization;
+
+namespace ResetPlayerPosOnDeath
+{
+    public class ResetPlayerPosOnDeathConfig : IRocketPluginConfiguration
+    {
+        public bool PluginEnabled;
+
+        [XmlArrayItem(ElementName = "SteamId")]
+        public List<string> SteamIds;
+
+        public string ServerPath;
+
+        public void LoadDefaults()
+        {
+            PluginEnabled = true;
+            SteamIds = new List<string>() { "place holder string" };
+            ServerPath = "change me";
+        }
+    }
+}
