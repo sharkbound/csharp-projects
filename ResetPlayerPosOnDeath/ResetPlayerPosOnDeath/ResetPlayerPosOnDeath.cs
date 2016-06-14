2b7b4d9a22eb4aa9cb78d7188983a366094787f9
sharkbound <laptopblaster@gmail.com>
2016-06-14 09:19:01 -0700

    updated libs for godmode announcer and made code changes for ResetPlayerPosOnDeath

--- ResetPlayerPosOnDeath/ResetPlayerPosOnDeath/ResetPlayerPosOnDeath.cs
+++ ResetPlayerPosOnDeath/ResetPlayerPosOnDeath/ResetPlayerPosOnDeath.cs
@@ -10,6 +10,7 @@ using Rocket.Unturned;
 using Rocket.Unturned.Player;
 using SDG.Unturned;
 using System.IO;
+using UnityEngine;
 
 namespace ResetPlayerPosOnDeath
 {
@@ -30,42 +31,67 @@ namespace ResetPlayerPosOnDeath
             Logger.Log("ResetPlayerPosOnDeath has loaded!");
             Logger.Log("ServerPath: " + ResetPlayerPosOnDeath.Instance.Configuration.Instance.ServerPath);
             Logger.Log("players Folder Path: " + playersFolderPath);
+
+          /*  Logger.LogError("==========================================");
+            Logger.LogError("provider.path (edited): " + getServerPath() );
+            Logger.LogError("=========================================="); */
         }
 
         protected override void Unload()
         {
             Logger.Log("ResetPlayerPosOnDeath has Unloaded!");
+            U.Events.OnPlayerDisconnected -= Events_OnPlayerDisconnected;
         }
 
         public void Events_OnPlayerDisconnected(UnturnedPlayer player)
         {
             string[] directories =  System.IO.Directory.GetDirectories(playersFolderPath);
 
-            bool playerIdIsFoundInList = false;
-
             foreach (string id in Instance.Configuration.Instance.SteamIds)
             {
                 if (player.Id == id)
                 {
-                    foreach (string s in directories)
+                    foreach (string dir in directories)
                     {
-                        if (s.Contains(player.Id))
+                        if (dir.Contains(player.Id))
                         {
                             //Logger.Log("found player directory for " + player.Id + " at:");
                             //Logger.Log(s);
 
-                            string deleteDir = s + @"\" + Provider.map + @"\Player\" + "Player.dat";
+                            string deleteDir = dir + @"\" + Provider.map + @"\Player\" + "Player.dat";
                             //Logger.LogError(deleteDir);
 
                             if (File.Exists(deleteDir))
                             {
                                 Logger.Log("deleting player.dat file for player:" + "SteamId- " + player.Id + " / name- " + player.DisplayName);
-                                File.Delete(s + @"\" + Provider.map + @"\Player\" + "Player.dat"); 
+                                File.Delete(dir + @"\" + Provider.map + @"\Player\" + "Player.dat"); 
+                            }
+                            else
+                            {
+                                Logger.LogError("Failed to find player.dat for player: name( " 
+                                    + player.DisplayName + " ) / id( " + player.Id + " )" 
+                                    + " at path( " + dir + " )");
                             }
                         }
                     }
                 }
             }
+        }
+
+        public string getServerPath()
+        {
+            string path;
+
+            //path = Provider.path;
+            path = string.Copy(Application.dataPath);
+            //path = Application.dataPath;
+
+            path = path.Remove(path.LastIndexOf('/'), path.LastIndexOf('a') - path.LastIndexOf('/'));
+            path = path.Remove(path.LastIndexOf('a'), 1);
+
+            path += "/Servers/";
+
+            return path;
         }
     }
 }
