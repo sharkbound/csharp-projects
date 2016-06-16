08d78631567fd1bfd39ad5ac9da777fddffa7279
sharkbound <laptopblaster@gmail.com>
2016-06-16 15:47:42 -0700

    added new plugin project easyAmmo, trying to work out a issue with it currently

---
+++ EasyAmmoRocketMod/EasyAmmoRocketMod/EasyAmmoConfig.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Rocket.API;
+
+namespace EasyAmmoRocketMod
+{
+    public class EasyAmmoConfig : IRocketPluginConfiguration
+    {
+       public bool PluginEnabled;
+
+        public void LoadDefaults()
+        {
+            PluginEnabled = true;
+        }
+    }
+}
