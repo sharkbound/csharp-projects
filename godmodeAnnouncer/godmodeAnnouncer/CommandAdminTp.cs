2be057fb918a9e8419d09acd7a2ab3846e928c4b
sharkbound <laptopblaster@gmail.com>
2016-07-12 18:40:23 -0700

    added 2 new commands

---
+++ godmodeAnnouncer/godmodeAnnouncer/CommandAdminTp.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Rocket.Unturned.Player;
+using Rocket.Core.Logging;
+using Rocket.API;
+using Rocket.Unturned.Chat;
+
+namespace godmodeAnnouncer
+{
+    class AdminTp : IRocketCommand
+    {
+        public List<string> Aliases
+        {
+            get
+            {
+                return new List<string> {"atp"};
+            }
+        }
+
+        public AllowedCaller AllowedCaller
+        {
+            get
+            {
+                return Rocket.API.AllowedCaller.Player;
+            }
+        }
+
+        public string Help
+        {
+            get
+            {
+                return "Vanishes and tp's you to a player";
+            }
+        }
+
+        public string Name
+        {
+            get
+            {
+                return "AdminTp";
+            }
+        }
+
+        public List<string> Permissions 
+        {
+            get
+            {
+                return new List<string> { "admintp" };
+            }
+        }
+
+        public string Syntax
+        {
+            get
+            {
+                return "<player>";
+            }
+        }
+
+        public void Execute(IRocketPlayer caller, string[] command)
+        {
+            if (command.Length != 1)
+            {
+                UnturnedChat.Say(caller, "You must enter a player name!");
+                return;
+            }
+
+            UnturnedPlayer player = (UnturnedPlayer)caller;
+            if (player == null)
+            {
+                return;
+            }
+
+            UnturnedPlayer otherPlayer = UnturnedPlayer.FromName(command[0]);
+            if (otherPlayer == null)
+            {
+                UnturnedChat.Say(caller, "Could not find player by the name of " + command[0]);
+                return;
+            }
+
+            player.Features.VanishMode = true;
+            player.Teleport(otherPlayer);
+
+            UnturnedChat.Say(caller, "You AdminTP'd to " + otherPlayer.DisplayName);
+            Logger.Log("Player " + player.DisplayName + " AdminTp'd to " + otherPlayer.DisplayName);
+        }
+    }
+}
---
+++ godmodeAnnouncer/godmodeAnnouncer/CommandVC.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Rocket.Unturned.Player;
+using Rocket.Core.Logging;
+using Rocket.API;
+using Rocket.Unturned.Chat;
+
+namespace godmodeAnnouncer
+{
+    class VC : IRocketCommand
+    {
+        public List<string> Aliases
+        {
+            get
+            {
+                return new List<string> ();
+            }
+        }
+
+        public AllowedCaller AllowedCaller
+        {
+            get
+            {
+                return Rocket.API.AllowedCaller.Both;
+            }
+        }
+
+        public string Help
+        {
+            get
+            {
+                return "Check if player is in vanish";
+            }
+        }
+
+        public string Name
+        {
+            get
+            {
+                return "vc";
+            }
+        }
+
+        public List<string> Permissions
+        {
+            get
+            {
+                return new List<string> { "vc" };
+            }
+        }
+
+        public string Syntax
+        {
+            get
+            {
+                return "<player>";
+            }
+        }
+
+        public void Execute(IRocketPlayer caller, string[] command)
+        {
+            if (command.Length != 1)
+            {
+                UnturnedChat.Say(caller, "You must enter a player name!");
+                return;
+            }
+
+            UnturnedPlayer otherPlayer = UnturnedPlayer.FromName(command[0]);
+            if (otherPlayer == null)
+            {
+                UnturnedChat.Say(caller, "Could not find player by the name of " + command[0]);
+                return;
+            }
+
+            if (otherPlayer.Features.VanishMode)
+            {
+                if (caller is ConsolePlayer)
+                {
+                    Logger.Log("The player " + otherPlayer.DisplayName + " is in vanish"); 
+                }
+                else
+                {
+                    Logger.Log("The player " + otherPlayer.DisplayName + " is in vanish"); 
+                    UnturnedChat.Say(caller, "The player " + otherPlayer.DisplayName + " is in vanish"); 
+                }
+            }
+            else
+            {
+                if (caller is ConsolePlayer)
+                {
+                    Logger.Log("The player " + otherPlayer.DisplayName + " is NOT vanish");
+                }
+                else
+                {
+                    Logger.Log("The player " + otherPlayer.DisplayName + " is NOT vanish");
+                    UnturnedChat.Say(caller, "The player " + otherPlayer.DisplayName + " is NOT vanish");
+                }
+            }
+        }
+    }
+}
