4630e46cdab30e2275bbf5ba8f76a2afd92df0f0
sharkbound <laptopblaster@gmail.com>
2017-02-10 14:48:38 -0800

    added asyncLearningExample

---
+++ AsyncLearningExample/ConsoleForTesting/SharkboundExtentions.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Diagnostics;
+using System.Text.RegularExpressions;
+
+namespace ConsoleForTesting
+{
+    public static class Extentions
+    {
+        public static string ToDigitsOnly(this string input)
+        {
+            return Regex.Replace(input, @"[^\d]", "");
+        }
+
+        public static string Filter(this string input)
+        {
+            return Regex.Replace(input, @"test|hi|justme", "");
+        }
+
+        public static double GetTotalSecondsElapsed(this DateTime dt)
+        {
+            return (DateTime.Now - dt).TotalSeconds;
+        }
+    }
+}
---
+++ ConsoleForTesting/ConsoleForTesting/Methods.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading.Tasks;
+
+namespace ConsoleForTesting
+{
+    internal class Methods
+    {
+        public async Task<double> GetTaskExecTimeAsync(Func<Task> task, ConsoleColor color = ConsoleColor.Green)
+        {
+            Stopwatch sw1 = new Stopwatch();
+
+            sw1.Start();
+            await task.Invoke();
+            sw1.Stop();
+
+            return (double)sw1.ElapsedMilliseconds / 1000;
+        }
+
+        public async Task TimeTaskExecAsync(double delay, string Idenifier)
+        {
+            double execSeconds = await GetTaskExecTimeAsync(async () =>
+            {
+                await Task.Delay((int)(delay * 1000));
+            });
+
+            CWriteLine($"{Idenifier} has finished after {execSeconds} seconds!", ConsoleColor.Green);
+        }
+
+        public void CWriteLine(string msg, ConsoleColor color)
+        {
+            ConsoleColor prevForeColor = Console.ForegroundColor;
+            Console.ForegroundColor = color;
+            Console.WriteLine(msg);
+            Console.ForegroundColor = prevForeColor;
+        }
+
+        public void CWrite(string msg, ConsoleColor color)
+        {
+            ConsoleColor prevForeColor = Console.ForegroundColor;
+            Console.ForegroundColor = color;
+            Console.Write(msg);
+            Console.ForegroundColor = prevForeColor;
+        }
+
+        public void Pause(string pauseMsg, ConsoleColor color = ConsoleColor.Gray)
+        {
+            CWriteLine(pauseMsg, color);
+            Console.ReadKey(true);
+        } 
+    }
+}
