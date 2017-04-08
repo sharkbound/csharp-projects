15ef12e7609a2d003ea5306854fe074f64dce099
sharkbound <laptopblaster@gmail.com>
2017-04-08 14:46:14 -0700

    added encryptionGUI

---
+++ EncryptionGUI/EncryptionGUI/MainWindow.xaml.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using EncryptionGUI.Util;
+
+namespace EncryptionGUI
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        #region methods
+        void EncryptText()
+        {
+            if (!string.IsNullOrWhiteSpace(txtEncrypt.Text))
+            {
+                txtDecrypt.Text = Encryiption.Encode(txtEncrypt.Text, "key_value", offset: 10_000);
+            }
+        }
+
+        void DecryptText()
+        {
+            if (!string.IsNullOrWhiteSpace(txtDecrypt.Text))
+            {
+                txtEncrypt.Text = Encryiption.Decode(txtDecrypt.Text, "key_value", offset: 10_000);
+            }
+        }
+        #endregion
+
+        private void btnEncrypt_Click(object sender, RoutedEventArgs e)
+        {
+            EncryptText();
+        }
+
+        private void txtEncrypt_KeyDown(object sender, KeyEventArgs e)
+        {
+            if(e.Key == Key.Enter)
+            {
+                EncryptText();
+            }
+        }
+
+        private void btnDecrypt_Click(object sender, RoutedEventArgs e)
+        {
+            DecryptText();
+        }
+
+        private void txtDecrypt_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Enter)
+            {
+                DecryptText();
+            }
+        }
+    }
+}
---
+++ EncryptionGUI/EncryptionGUI/util/Encryiption.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EncryptionGUI.Util
+{
+    class Encryiption
+    {
+        public static string Encode(string text, string key, int offset = 0)
+        {
+            while (key.Length < text.Length)
+            {
+                key += key;
+            }
+
+            string encoded = "";
+            for (int i = 0; i < text.Length; i++)
+            {
+                char encodedChar = (char)((int)text[i] + (int)key[i] % 256 + i + offset);
+                encoded += encodedChar;
+            }
+
+            return encoded;
+        }
+
+        public static string Decode(string text, string key, int offset = 0)
+        {
+            while (key.Length < text.Length)
+            {
+                key += key;
+            }
+
+            string decoded = "";
+            for (int i = 0; i < text.Length; i++)
+            {
+                char decodedChar = (char)((int)text[i] - (int)key[i] % 256 - i - offset);
+                decoded += decodedChar;
+            }
+
+            return decoded;
+        }
+    }
+}
