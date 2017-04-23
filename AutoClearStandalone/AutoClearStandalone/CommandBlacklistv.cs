749ab61d35edb01eff36660052e56d4a802fd42c
sharkbound <laptopblaster@gmail.com>
2017-04-22 18:56:34 -0700

    ...

--- AutoClearStandalone/AutoClearStandalone/CommandBlacklistv.cs
+++ AutoClearStandalone/AutoClearStandalone/CommandBlacklistv.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 using Rocket.API;
 using Rocket.Unturned.Player;
 using Rocket.Unturned.Chat;
