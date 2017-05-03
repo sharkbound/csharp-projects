56379ff70fcc2ecc09ae1ead71a62eb018128d33
sharkbound <laptopblaster@gmail.com>
2017-05-03 14:44:20 -0700

    added foodstatdumpers plugin

---
+++ FoodItemInfoDumper/Config.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Rocket.API;
+
+namespace AdminToolz
+{
+    public class Config : IRocketPluginConfiguration
+    {
+        public string placeholder;
+
+        public void LoadDefaults()
+        {
+            placeholder = "placeholder";
+        }
+    }
+}
---
+++ dotnet47/dotnet47/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace dotnet47
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            new Program().Start();
+        }
+
+        Random r = new Random();
+        char[] _AsciiLetters = Enumerable.Range('a', 'z' - 'a' + 1).Select(i => (char)i).ToArray();
+
+        (string name, int age) getPerson()
+        {
+            string retName = "";
+            for (int i = 0; i < 20; i++)
+            {
+                retName += _AsciiLetters[r.Next(0, _AsciiLetters.Length)];
+            }
+
+            return (retName, r.Next(0, 100));
+        }
+
+        private void Start()
+        {
+
+            var info = getPerson();
+            Console.WriteLine($"{info.name} : {info.age}");
+
+            foreach (char c in _AsciiLetters)
+            {
+                Console.Write(c);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
